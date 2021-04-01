5635c7d3e91d105364ce64b6612bab914e16516c
nightlyside <nightlyside@gmail.com>
2021-04-01 14:00:08 +0200

    Walrus operator

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,11 @@
 # Trying to test whether I can push code
 print("Hello, world! 😁")
 print("Coucou")
+
+
+import random
+# exemple de walrus operator pour compter jusqu'a 10
+while (compteur := random.randrange(11)):
+    if compteur == 10:
+        print("10")
+        break
